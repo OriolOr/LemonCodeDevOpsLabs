a5844fe39e051834648fbc13e4f6828f5c85b2bd
Oriol <oriol.orus.or@gmail.com>
2022-11-22 22:44:40 +0100

    update ejercicio4.sh linuxlab

--- LinuxLab/ejercicio4.sh
+++ LinuxLab/ejercicio4.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 #search in url 
-if [ $# == 2 ]; then
+if [ $# -eq 2 ]; then
+    #get web info
     curl -s $1 -o webFile.txt
+    
+    #file parse
     nWords=$(curl -s $1 | grep -io $2 | wc -w);
-    nLine=23
-    if [$nWords == 0]; then
-        echo "No se ha encontrado la palabra $1"
-    else 
+    nLine=$(grep -c $2 webFile.txt);
+
+    if [ $nWords -eq 1 ]; then
+        echo "La palabra $2 aparece 1 vez"
+        echo "Aparece únicamente en la linea $nLine"
+    elif [ $nWords -gt 1 ]; then
         echo "La palabra $2 aprarece $nWords veces"
         echo "Aparece por primera vez en la línea $nLine"
+    else 
+        echo "No se ha encontrado la palabra $2"
     fi
 else
     echo "Se necesitan únicamente dos parámetros para ejecutar este script"
 fi
+
